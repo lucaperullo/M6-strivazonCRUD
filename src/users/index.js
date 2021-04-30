b8fbe4e2ecf26dcca1c02b444124dc7cb54ce391
luca <lucaperullo@outlook.it>
2021-04-30 11:18:01 +0200

    up

---
+++ src/users/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import UserSchema from "./schema.js";
+
+const userRouter = express.Router();
+
+userRouter.get("/", async (req, res) => {
+  try {
+    if (req.query.name) {
+      const user = await userSchema.findOne({ name: req.query.name });
+      if (user) {
+        res.status(200).send(user);
+      } else {
+        res.status(404).send("user not found");
+      }
+    } else {
+      const allUsers = await userSchema.find();
+      res.status(200).send(allUsers);
+    }
+  } catch (error) {
+    res.send({ message: error });
+  }
+});
+
+userRouter.post("/", async (req, res) => {
+  try {
+    const newUser = new UserSchema();
+  } catch (error) {}
+});
