8c98028fd5e51c685361bf01e3cd475fcd466d3f
luca <lucaperullo@outlook.it>
2021-04-30 14:08:16 +0200

    uppy

---
+++ src/products/index.js
@@ -0,0 +1,85 @@
+import express from "express";
+import ProductSchema from "./product-Schema.js";
+import ReviewsSchema from "../reviews/reviews-Schema.js";
+import UserSchema from "../users/schema.js";
+
+const router = express.Router();
+
+router.post("/:ProductID/reviews", async (req, res, next) => {
+  try {
+    const newReview = new ReviewsSchema(req.body);
+    const { _id } = await newReview.save();
+    res.status(201).send(_id);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//GET ALL REVIEWS FOR A PRODUCT
+router.get("/:ProductID/reviews", async (req, res, next) => {
+  try {
+    const ProductID = req.params.ProductID;
+    const reviews = await ProductSchema.findById(ProductID);
+    res.send(reviews);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//GET A SINGULAR REVIEW
+router.get("/:ProductID/reviews/:reviewID", async (req, res, next) => {
+  try {
+    const ProductID = req.params.ProductID;
+    const reviewID = req.params.ReviewID;
+    const review = await ProductSchema.find;
+  } catch (error) {
+    next(error);
+  }
+});
+
+//POST A REVIEW
+// router.post("/:ProductID/review/:userID", async (req, res, next) => {
+//   try {
+//     await UserSchema.addReviewIdToProduct(req.params.)
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+//MODIFY A REVIEW
+router.put("/:ProductID/reviews/:reviewID", async (req, res, next) => {
+  try {
+    const modifiedReview = await ReviewModel.findByIdAndUpdate(
+      req.params.reviewID,
+      req.body,
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+    if (modifiedReview) {
+      res.send(modifiedReview);
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+//DELETE A REVIEW
+router.delete("/:ProductID/reviews/:reviewID", async (req, res, next) => {
+  try {
+    const review = await ReviewModel.findByIdAndDelete(req.params.reviewID);
+    if (review) {
+      res.send(review);
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+export default router;
--- src/products/productControllers.js
+++ src/products/productControllers.js
@@ -1,92 +1,92 @@
-import ProductSchema from "./schema-Product.js"
+import ProductSchema from "./product-Schema.js";
 // import mongoose from "mongoose";
 import q2m from "query-to-mongo";
 
 export const getProducts = async (req, res, next) => {
-    try {
-      const { criteria, options, links } = q2m(req.query);
+  try {
+    const { criteria, options, links } = q2m(req.query);
     //   const query = q2m(req.query);
 
-      const total = await ProductSchema.countDocuments();
-  
-      const products = await ProductSchema.find(criteria, options.fields)
-        .sort(options.sort)
-        .skip(options.skip)
-        .limit(options.limit);
-      res.send({
-        success: true,
-        links: links('/products', total),
-        data: products,
-      });
-    } catch (error) {
+    const total = await ProductSchema.countDocuments();
+
+    const products = await ProductSchema.find(criteria, options.fields)
+      .sort(options.sort)
+      .skip(options.skip)
+      .limit(options.limit);
+    res.send({
+      success: true,
+      links: links("/products", total),
+      data: products,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getProductById = async (req, res, next) => {
+  try {
+    const product = await ProductModel.findById(req.params.id);
+    if (product) {
+      res.status(200).send(product);
+    } else {
+      const error = new Error();
+      error.statusCode = 404;
       next(error);
     }
-  };
+  } catch (error) {
+    console.log(error);
+    next("While reading products list a problem occurred!", error);
+  }
+};
 
-  export const getProductById = async (req,res,next) =>{
-    try {
-        const product = await ProductModel.findById(req.params.id);
-        if (product) {
-          res.status(200).send(product);
-        } else {
-          const error = new Error();
-          error.statusCode = 404;
-          next(error);
-        }
-      } catch (error) {
-        console.log(error);
-        next("While reading products list a problem occurred!", error);
+export const postProduct = async (req, res, next) => {
+  try {
+    const newProduct = {
+      ...req.body,
+    };
+    const savedProduct = await ProductModel.create(newProduct);
+    const { _id } = savedProduct;
+
+    res.send({ message: "Product Created with this ID =>", _id });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteProduct = async (req, res, next) => {
+  try {
+    const product = await ProductModel.findByIdAndDelete(req.params.id);
+    if (product) {
+      res.status(204).send();
+    } else {
+      const error = new Error(`Product with id ${req.params.id} not found`);
+      error.statusCode = 404;
+      next(error);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const editProduct = async (req, res, next) => {
+  try {
+    const newProduct = { ...req.body };
+    const product = await ProductModel.findByIdAndUpdate(
+      req.params.id,
+      newProduct,
+      {
+        runValidators: true,
+        new: true,
       }
-    }
-
-  export const postProduct = async (req, res, next) => {
-    try {
-      const newProduct = {
-        ...req.body,
-      };
-      const savedProduct = await ProductModel.create(newProduct);
-      const { _id } = savedProduct;
-
-      res.send({ message: "Product Created with this ID =>", _id });
-    } catch (error) {
+    );
+    if (product) {
+      res.send(product);
+    } else {
+      const error = new Error(`Product with id ${req.params.id} not found`);
+      error.statusCode = 404;
       next(error);
     }
-  };
-
-  export const deleteProduct = async (req, res, next) => {
-    try {
-      const product = await ProductModel.findByIdAndDelete(req.params.id);
-      if (product) {
-          res.status(204).send();
-      }else{
-        const error = new Error(`Product with id ${req.params.id} not found`);
-        error.statusCode = 404;
-        next(error);
-      }
-    } catch (error) {
-      next(error);
-    }
-  };
-  
-  export const editProduct = async (req, res, next) => {
-    try {
-      const newProduct = { ...req.body};
-      const product = await ProductModel.findByIdAndUpdate(
-        req.params.id,
-        newProduct,
-        {
-          runValidators: true,
-          new: true,
-        }
-      );
-      if (product) {
-        res.send(product);
-      } else {
-        const error = new Error(`Product with id ${req.params.id} not found`);
-        error.statusCode = 404;
-        next(error);
-      }
-    } catch (error) {
-      next(error);
-    }
-  };
+  } catch (error) {
+    next(error);
+  }
+};
